bb3b0f1ef062ad149c0ee35ed992879c482f78f4
hyune <choi8608@gmail.com>
2022-07-01 12:49:52 +0900

    Initial commit

---
+++ src/main/java/com/example/travelermileageservice/TravelerMileageServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.travelermileageservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TravelerMileageServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TravelerMileageServiceApplication.class, args);
+	}
+
+}
