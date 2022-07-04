0960357c84b023cf8e25f853808b0fbdf2edfca4
hyune <choi8608@gmail.com>
2022-07-04 22:56:36 +0900

    :white_check_mark: 리뷰 수정 테스트 케이스 추가

--- src/test/java/com/example/travelermileageservice/LargeTest.java
+++ src/test/java/com/example/travelermileageservice/LargeTest.java
@@ -1,11 +1,13 @@
-package com.example.travelermileageservice.domain.review;
+package com.example.travelermileageservice;
 
 import com.example.travelermileageservice.domain.point.service.PointGetService;
-import com.example.travelermileageservice.domain.review.entity.AttachedPhoto;
+import com.example.travelermileageservice.domain.review.entity.Review;
 import com.example.travelermileageservice.domain.review.repository.ReviewRepository;
 import com.example.travelermileageservice.domain.review.service.ReviewAddService;
 import com.example.travelermileageservice.domain.review.service.ReviewDeleteService;
+import com.example.travelermileageservice.domain.review.service.ReviewModService;
 import com.example.travelermileageservice.domain.review.service.dto.ReviewAddDto;
+import com.example.travelermileageservice.domain.review.service.dto.ReviewModDto;
 import com.example.travelermileageservice.domain.review.service.exception.ReviewAddException;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.DynamicTest;
@@ -13,6 +15,7 @@ import org.junit.jupiter.api.TestFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
 import java.util.stream.Stream;
@@ -22,10 +25,13 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.junit.jupiter.api.DynamicTest.dynamicTest;
 
 @SpringBootTest
-class ReviewServiceLargeTest {
+class LargeTest {
 
     @Autowired
     private ReviewAddService reviewAddService;
+
+    @Autowired
+    private ReviewModService reviewModService;
 
     @Autowired
     private ReviewDeleteService reviewDeleteService;
@@ -44,20 +50,35 @@ class ReviewServiceLargeTest {
         final UUID user1 = UUID.randomUUID();
         final UUID user2 = UUID.randomUUID();
         final UUID placeId = UUID.randomUUID();
-        final List<AttachedPhoto> attachedPhotos = List.of(
-                new AttachedPhoto(UUID.randomUUID()),
-                new AttachedPhoto(UUID.randomUUID()));
+        final List<UUID> attachedPhotos = List.of(UUID.randomUUID(), UUID.randomUUID());
 
         return Stream.of(
                 dynamicTest("user1이 리뷰 작성", () -> {
                     // given
-                    final ReviewAddDto reviewAddDto = ReviewAddDto.of(reviewId, content, List.of(UUID.randomUUID(), UUID.randomUUID()), user1, placeId);
+                    final ReviewAddDto reviewAddDto = ReviewAddDto.of(reviewId, content, attachedPhotos, user1, placeId);
 
                     // when
                     reviewAddService.add(reviewAddDto);
 
                     // then
                     assertThat(reviewRepository.findById(reviewId)).isNotNull();
+                    assertThat(pointGetService.get(user1)).isEqualTo(2);
+                }),
+
+                dynamicTest("user1이 작성한 리뷰에 내용 변경 & 사진 1장 추가", () -> {
+                    // given
+                    final String newContent = "변경된 좋아요!";
+                    final List<UUID> newAttachedPhotos = new ArrayList<>(attachedPhotos);
+                    newAttachedPhotos.add(UUID.randomUUID());
+                    final ReviewModDto reviewModDto = ReviewModDto.of(reviewId, newContent, newAttachedPhotos);
+
+                    // when
+                    reviewModService.mod(reviewModDto);
+
+                    // then
+                    final Review review = reviewRepository.findById(reviewId).get();
+                    assertThat(review.getContent()).isEqualTo(newContent);
+                    assertThat(review.getAttachedPhotos()).hasSize(3);
                     assertThat(pointGetService.get(user1)).isEqualTo(2);
                 }),
 
@@ -70,7 +91,6 @@ class ReviewServiceLargeTest {
                             .isInstanceOf(ReviewAddException.class);
 
                     // then
-
                 }),
 
                 dynamicTest("user1이 작성한 리뷰 삭제", () -> {
