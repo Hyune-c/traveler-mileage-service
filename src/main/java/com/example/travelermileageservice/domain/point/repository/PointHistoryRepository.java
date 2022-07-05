7d457d05ac1d510199d46f5cd7fbbe2b274f1ad4
hyune <choi8608@gmail.com>
2022-07-06 03:10:16 +0900

    :white_check_mark: 리뷰 수정 테스트 케이스 추가

--- src/main/java/com/example/travelermileageservice/domain/point/repository/PointHistoryRepository.java
+++ src/main/java/com/example/travelermileageservice/domain/point/repository/PointHistoryRepository.java
@@ -5,10 +5,14 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
-import java.util.UUID;
 
 @Repository
 public interface PointHistoryRepository extends JpaRepository<PointHistory, Long>, PointHistoryQuerydsl {
 
-    Optional<PointHistory> findByTypeAndSourceId(PointHistory.Type type, UUID sourceId);
+    /**
+     * Large Test를 위한 기능
+     *
+     * @return 가장 마지막에 작성된 이력
+     */
+    Optional<PointHistory> findTopByOrderByIdDesc();
 }
--- src/test/java/com/example/travelermileageservice/LargeTest.java
+++ src/test/java/com/example/travelermileageservice/LargeTest.java
@@ -1,7 +1,9 @@
 package com.example.travelermileageservice;
 
+import com.example.travelermileageservice.domain.point.repository.PointHistoryRepository;
 import com.example.travelermileageservice.domain.point.service.PointGetService;
 import com.example.travelermileageservice.domain.review.entity.Review;
+import com.example.travelermileageservice.domain.review.repository.AttachedPhotoRepository;
 import com.example.travelermileageservice.domain.review.repository.ReviewRepository;
 import com.example.travelermileageservice.domain.review.service.ReviewAddService;
 import com.example.travelermileageservice.domain.review.service.ReviewDeleteService;
@@ -9,6 +11,7 @@ import com.example.travelermileageservice.domain.review.service.ReviewModService
 import com.example.travelermileageservice.domain.review.service.dto.ReviewAddDto;
 import com.example.travelermileageservice.domain.review.service.dto.ReviewModDto;
 import com.example.travelermileageservice.domain.review.service.exception.ReviewAddException;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.DynamicTest;
 import org.junit.jupiter.api.TestFactory;
@@ -42,9 +45,22 @@ class LargeTest {
     @Autowired
     private ReviewRepository reviewRepository;
 
-    @DisplayName("리뷰 작성")
+    @Autowired
+    private AttachedPhotoRepository attachedPhotoRepository;
+
+    @Autowired
+    private PointHistoryRepository pointHistoryRepository;
+
+    @AfterEach
+    public void afterEach() {
+        reviewRepository.deleteAll();
+        attachedPhotoRepository.deleteAll();
+        pointHistoryRepository.deleteAll();
+    }
+
+    @DisplayName("리뷰 작성과 포인트 변경")
     @TestFactory
-    Stream<DynamicTest> dynamicTestsFromCollection() {
+    Stream<DynamicTest> writeAReviewAndChangePoints() {
         final String content = "좋아요!";
         final UUID reviewId = UUID.randomUUID();
         final UUID user1 = UUID.randomUUID();
@@ -62,23 +78,6 @@ class LargeTest {
 
                     // then
                     assertThat(reviewRepository.findById(reviewId)).isNotNull();
-                    assertThat(pointGetService.get(user1)).isEqualTo(3);
-                }),
-
-                dynamicTest("user1이 작성한 리뷰에 내용 변경 & 사진 1장 추가", () -> {
-                    // given
-                    final String newContent = "변경된 좋아요!";
-                    final List<UUID> newAttachedPhotos = new ArrayList<>(attachedPhotos);
-                    newAttachedPhotos.add(UUID.randomUUID());
-                    final ReviewModDto reviewModDto = ReviewModDto.of(reviewId, newContent, newAttachedPhotos);
-
-                    // when
-                    reviewModService.mod(reviewModDto);
-
-                    // then
-                    final Review review = reviewRepository.findById(reviewId).get();
-                    assertThat(review.getContent()).isEqualTo(newContent);
-                    assertThat(review.getAttachedPhotos()).hasSize(3);
                     assertThat(pointGetService.get(user1)).isEqualTo(3);
                 }),
 
@@ -137,6 +136,120 @@ class LargeTest {
 
                     // then
                     assertThat(pointGetService.get(user2)).isEqualTo(5);
+                })
+        );
+    }
+
+    @DisplayName("리뷰 수정과 포인트 변경")
+    @TestFactory
+    Stream<DynamicTest> editReviewAndChangePoints() {
+        final String content = "좋아요!";
+        final UUID reviewId = UUID.randomUUID();
+        final UUID user1 = UUID.randomUUID();
+        final UUID user2 = UUID.randomUUID();
+        final UUID placeId = UUID.randomUUID();
+        final List<UUID> attachedPhotos = List.of(UUID.randomUUID(), UUID.randomUUID());
+
+        return Stream.of(
+                dynamicTest("내용(1) + 사진(1) + 최초(1) = 3점", () -> {
+                    // given
+                    final ReviewAddDto reviewAddDto = ReviewAddDto.of(reviewId, content, attachedPhotos, user1, placeId);
+
+                    // when
+                    reviewAddService.add(reviewAddDto);
+
+                    // then
+                    assertThat(reviewRepository.findById(reviewId)).isNotNull();
+                    assertThat(pointGetService.get(user1)).isEqualTo(3);
+                }),
+
+                dynamicTest("기존(3) + 내용 변경(0) + 사진 1장 추가(0) = 3점", () -> {
+                    // given
+                    final String newContent = "변경된 좋아요!";
+                    final List<UUID> newAttachedPhotos = new ArrayList<>(attachedPhotos);
+                    newAttachedPhotos.add(UUID.randomUUID());
+                    final ReviewModDto reviewModDto = ReviewModDto.of(reviewId, newContent, newAttachedPhotos);
+
+                    // when
+                    reviewModService.mod(reviewModDto);
+
+                    // then
+                    final Review review = reviewRepository.findById(reviewId).get();
+                    assertThat(review.getContent()).isEqualTo(newContent);
+                    assertThat(review.getAttachedPhotos()).hasSize(3);
+                    assertThat(pointGetService.get(user1)).isEqualTo(3);
+                }),
+
+                dynamicTest("기존(3) + 사진 비우기(-1) = 2점", () -> {
+                    // given
+                    final String newContent = "변경된 좋아요!";
+                    final List<UUID> newAttachedPhotos = List.of();
+                    final ReviewModDto reviewModDto = ReviewModDto.of(reviewId, newContent, newAttachedPhotos);
+
+                    // when
+                    reviewModService.mod(reviewModDto);
+
+                    // then
+                    final Review review = reviewRepository.findById(reviewId).get();
+                    assertThat(review.getContent()).isEqualTo(newContent);
+                    assertThat(review.getAttachedPhotos()).isEmpty();
+
+                    assertThat(pointGetService.get(user1)).isEqualTo(2);
+                    assertThat(pointHistoryRepository.findTopByOrderByIdDesc().get().getPoint()).isEqualTo(-1);
+                }),
+
+                dynamicTest("기존(2) + 내용 비우기(-1) + 사진(1) = 2점", () -> {
+                    // given
+                    final String newContent = "";
+                    final List<UUID> newAttachedPhotos = List.of(UUID.randomUUID());
+                    final ReviewModDto reviewModDto = ReviewModDto.of(reviewId, newContent, newAttachedPhotos);
+
+                    // when
+                    reviewModService.mod(reviewModDto);
+
+                    // then
+                    final Review review = reviewRepository.findById(reviewId).get();
+                    assertThat(review.getContent()).isEqualTo(newContent);
+                    assertThat(review.getAttachedPhotos()).hasSize(1);
+
+                    assertThat(pointGetService.get(user1)).isEqualTo(2);
+                    assertThat(pointHistoryRepository.findTopByOrderByIdDesc().get().getPoint()).isZero();
+                }),
+
+                dynamicTest("기존(2) + 사진 비우기(-1) = 1점", () -> {
+                    // given
+                    final String newContent = "";
+                    final List<UUID> newAttachedPhotos = List.of();
+                    final ReviewModDto reviewModDto = ReviewModDto.of(reviewId, newContent, newAttachedPhotos);
+
+                    // when
+                    reviewModService.mod(reviewModDto);
+
+                    // then
+                    final Review review = reviewRepository.findById(reviewId).get();
+                    assertThat(review.getContent()).isEqualTo(newContent);
+                    assertThat(review.getAttachedPhotos()).isEmpty();
+
+                    assertThat(pointGetService.get(user1)).isEqualTo(1);
+                    assertThat(pointHistoryRepository.findTopByOrderByIdDesc().get().getPoint()).isEqualTo(-1);
+                }),
+
+                dynamicTest("기존(1) + 내용 채우기(1) + 사진 채우기(1) = 3점", () -> {
+                    // given
+                    final String newContent = "좋아요!!";
+                    final List<UUID> newAttachedPhotos = List.of(UUID.randomUUID(), UUID.randomUUID());
+                    final ReviewModDto reviewModDto = ReviewModDto.of(reviewId, newContent, newAttachedPhotos);
+
+                    // when
+                    reviewModService.mod(reviewModDto);
+
+                    // then
+                    final Review review = reviewRepository.findById(reviewId).get();
+                    assertThat(review.getContent()).isEqualTo(newContent);
+                    assertThat(review.getAttachedPhotos()).hasSize(2);
+
+                    assertThat(pointGetService.get(user1)).isEqualTo(3);
+                    assertThat(pointHistoryRepository.findTopByOrderByIdDesc().get().getPoint()).isEqualTo(2);
                 })
         );
     }
