ad444633b8ec8a4834ea8e8417318efa86ae1a14
hyune <choi8608@gmail.com>
2022-07-08 20:37:00 +0900

    :white_check_mark: 테스트 데이터 정리

--- src/test/java/com/example/travelermileageservice/domain/review/repository/ReviewRepositoryTest.java
+++ src/test/java/com/example/travelermileageservice/domain/review/repository/ReviewRepositoryTest.java
@@ -31,12 +31,10 @@ class ReviewRepositoryTest {
     void save() {
         // given
         final String content = "좋아요!";
-        final UUID reviewId = UUID.fromString("240a0658-dc5f-4878-9381-ebb7b2667772");
-        final UUID userId = UUID.fromString("3ede0ef2-92b7-4817-a5f3-0c575361f745");
-        final UUID placeId = UUID.fromString("2e4baf1c-5acb-4efb-a1af-eddada31b00f");
-        final List<AttachedPhoto> attachedPhotos = List.of(
-                new AttachedPhoto(UUID.fromString("e4d1a64e-a531-46de-88d0-ff0ed70c0bb8")),
-                new AttachedPhoto(UUID.fromString("afb0cef2-851d-4a50-bb07-9cc15cbdc332")));
+        final UUID reviewId = UUID.randomUUID();
+        final UUID userId = UUID.randomUUID();
+        final UUID placeId = UUID.randomUUID();
+        final List<AttachedPhoto> attachedPhotos = List.of(new AttachedPhoto(UUID.randomUUID()), new AttachedPhoto(UUID.randomUUID()));
 
         final Review review = new Review(reviewId, userId, content, placeId, attachedPhotos);
 
@@ -56,12 +54,10 @@ class ReviewRepositoryTest {
     void deleteAttachedPhoto() {
         // given
         final String content = "좋아요!";
-        final UUID reviewId = UUID.fromString("240a0658-dc5f-4878-9381-ebb7b2667772");
-        final UUID userId = UUID.fromString("3ede0ef2-92b7-4817-a5f3-0c575361f745");
-        final UUID placeId = UUID.fromString("2e4baf1c-5acb-4efb-a1af-eddada31b00f");
-        final List<AttachedPhoto> attachedPhotos = List.of(
-                new AttachedPhoto(UUID.fromString("e4d1a64e-a531-46de-88d0-ff0ed70c0bb8")),
-                new AttachedPhoto(UUID.fromString("afb0cef2-851d-4a50-bb07-9cc15cbdc332")));
+        final UUID reviewId = UUID.randomUUID();
+        final UUID userId = UUID.randomUUID();
+        final UUID placeId = UUID.randomUUID();
+        final List<AttachedPhoto> attachedPhotos = List.of(new AttachedPhoto(UUID.randomUUID()), new AttachedPhoto(UUID.randomUUID()));
 
         final Review review = new Review(reviewId, userId, content, placeId, attachedPhotos);
         reviewRepository.save(review);
